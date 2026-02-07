15adb3f7486a915693690e7800f75c2c95d99a93
Its-ech <its.ech.t9@gmail.com>
2026-02-07 18:16:24 +0330

    add 3 endpoints with router-dom@6.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter,Router, Route, } from 'react-router-dom'
 import './App.css';
+
+import Home from './pages/Home/Home'
+import Serch from './pages/Serch/Serch'
+import Sports from './pages/Sports/Sports'
 
 function App() {
   return (
     <div className="App">
-      
+
+      {/* End-points */}
+      <BrowserRouter>
+      <Routes>
+        <route path = '/' element ={<Home/>}/>
+        <route path = '/Serch' element ={<Serch/>}/>
+        <route path = '/Sports/:id' element ={<Sports/>}/>
+      </Routes> 
+      </BrowserRouter>
+
     </div>
   );
 }
