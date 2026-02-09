5aefd2b047cc9396c89f9adb3c3184a938bb8f07
Its-ech <its.ech.t9@gmail.com>
2026-02-09 21:25:59 +0330

    add sport/id information step 1

--- src/pages/Sports/Sports.js
+++ src/pages/Sports/Sports.js
@@ -1,7 +1,17 @@
+import {useParams} from 'react-router-dom'
+import {useFetch} from './../../hooks/useFetch'
+
 import React from 'react'
 
 export default function Sports() {
+  const{ id } = useParams()
+  const url ='http://localhost:3000/Sports/'+ id
+  const { data:Sport, isLoading,error } = useFetch(url)
   return (
-    <div>Sports</div>
+    <div className='sport'>
+      {error && <p className='error'>{error}</p>}
+      {isLoading && <p className='loading'>loading...</p>}
+      {Sport && <h1>{Sport.name}</h1>}
+    </div>
   )
 }
