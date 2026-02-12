2d9445165d75d3ca407ed08025200d4248a60d82
Its-ech <its.ech.t9@gmail.com>
2026-02-13 00:30:15 +0330

    add error <p> && affter new post you be redirect to home page

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Create.css'
 import { useFetch } from '../../hooks/useFetch'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function Create() {
@@ -8,50 +9,59 @@ export default function Create() {
     const[description, setDescription] = useState('')
     const[benefits, setBenefits] = useState('')
     const[equipment, setEquipment] = useState('')
+    const navigate = useNavigate ()
 
     const {postData , data , error} = useFetch('http://localhost:3000/Sports' , 'POST')
     const handleSubmit = (e)=>{
         e.preventDefault()
         postData({name,description,benefits,equipment})
     }
+
+    useEffect(()=> {
+        if (data) {
+            navigate('/')
+        }
+    },[data, navigate])
+
   return (
     <div className='create'>
         <h2 className='add-sport'>ADD New Sport</h2>
 
         <form onSubmit={handleSubmit}>
-            <lable>
+            <label>
                 <span>sport Name:</span>
                 <input type="text" onChange={(e)=> setName(e.target.value)}
                 value={name}
                 required
                 />
-            </lable>
+            </label>
 
-            <lable>
+            <label>
                 <span>sport description:</span>
                 <textarea type="text" onChange={(e)=> setDescription(e.target.value)}
                 value={description}
                 required
                 />
-            </lable>
+            </label>
 
-            <lable>
+            <label>
                 <span>sport benefits:</span>
                 <input type="text" onChange={(e)=> setBenefits(e.target.value)}
                 value={benefits}
                 required
                 />
-            </lable>
+            </label>
 
-            <lable>
+            <label>
                 <span>sport equipment:</span>
                 <input type="text" onChange={(e)=> setEquipment(e.target.value)}
                 value={equipment}
                 required
                 />
-            </lable>
+            </label>
             
             <button className="btn"> submit</button>
+            {error && <p className="error-msg">{error}</p>}
         </form>
     </div>
   )
