4ff148f86e2822330caec626919b5380841c7afe
Its-ech <its.ech.t9@gmail.com>
2026-02-13 07:55:03 +0330

    complite serch bar and done my first web app whit react

--- src/pages/Serch/Serch.js
+++ src/pages/Serch/Serch.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom'
+import '../Serch/Serch.css'
+import '../../components/SportList.css'
+import { useFetch } from '../../hooks/useFetch'
+import SportList from '../../components/SportList'
 
 export default function Serch() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('q')
+  const url = `http://localhost:3000/Sports?name=${query}`
+  
+  const {error, isLoading, data} = useFetch(url)
   return (
-    <div>Serch</div>
+    <div>
+      <h2 className='page-name'>
+        Sport including "{query}"</h2>
+        {error && <p>{error}</p>}
+        {isLoading && <p>isLoading...</p>}
+        {data && <SportList Sports={data}/>}
+    </div>
   )
 }
